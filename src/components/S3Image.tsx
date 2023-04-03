049b8ce58c8ffb1ebef3e33a2f38cb834640f071
aaron-visschedijk <aaron.visschedijk@gmail.com>
2023-04-03 15:23:13 +0200

    image from s3 component

--- src/components/S3Image.tsx
+++ src/components/S3Image.tsx
@@ -1,11 +1,12 @@
 
 const MEDIA_BUCKET = "https://django-portfolio-media-files.s3.eu-central-1.amazonaws.com/"
 
-export default function S3Image(props: { src: string }) {
+export default function S3Image(props: { src: string, className?: string }) {
     return (
         <img 
         src={MEDIA_BUCKET + props.src}
         alt="new"
+        className={props.className}
       />
     )
 }
