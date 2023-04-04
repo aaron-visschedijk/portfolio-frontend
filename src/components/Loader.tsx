5ed5c2f46686a2499452d880b460fe15be6eaa1d
aaron-visschedijk <aaron.visschedijk@gmail.com>
2023-04-04 06:31:44 +0200

    fix fallback not showing

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import styles from "./styles/Loader.module.css"
+import spinner from '../spinner.svg'
 
 type LoaderProps<T> = {
     request: Promise<T>;
@@ -7,20 +8,27 @@ type LoaderProps<T> = {
     fallback?: React.ReactNode;
 };
 
-function Loader<T>({ request, children, fallback = <Fallback/> }: LoaderProps<T>) {
+function Loader<T>({ request, children, fallback = <Fallback /> }: LoaderProps<T>) {
     const [data, setData] = React.useState<T | null>(null);
 
     useEffect(() => {
-        request.then((data) => setData(data));
+        request.then((data) => {
+            setData(data);
+        });
     }, [request]);
 
-    return <>{data ? children(data as T) : fallback}</>;
+    return <div>
+        {!data && fallback}
+        <div className={data ? styles.done : styles.not} >
+            {data ? children(data as T) : fallback}
+        </div>
+    </div>;
 }
 
 
 function Fallback() {
     return <div className={styles.spinner}>
-        <div>Hello World</div>
+        <h3>Hold on a sec...</h3>
     </div>;
 }
 
